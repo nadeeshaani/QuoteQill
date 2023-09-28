6b004239b22012705c820fa7688a29ccef57acaa
Nadeeshani Nawarathne <nawarat-se19043@stu.kln.ac.lk>
2023-09-28 16:51:55 +0530

    added the project files

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuoteQill.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuoteQill.Startup))]
+namespace QuoteQill
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
